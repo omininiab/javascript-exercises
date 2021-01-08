b5f5df9d87ae3bc3c776129a0cfc0489fdf5be87
Ominini <omininiab@gmail.com>
2021-01-08 15:50:55 -0500

    completed task 7: tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,14 @@
-const ftoc = function() {
+const ftoc = function (f) {
+	c = ((f - 32) * 5) / 9;
+	return Number(c.toFixed(1));
+};
 
-}
-
-const ctof = function() {
-
-}
+const ctof = function (c) {
+	f = (c * 9) / 5 + 32;
+	return Number(f.toFixed(1));
+};
 
 module.exports = {
-  ftoc,
-  ctof
-}
+	ftoc,
+	ctof,
+};
