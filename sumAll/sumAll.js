631d44a8d215bb832f9b168691de5b6bebc82123
Ominini <omininiab@gmail.com>
2021-01-08 15:04:00 -0500

    completed task 5: sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumUp = (n) => (n * (n + 1)) / 2;
 
-}
+const sumAll = function (num1, num2) {
+	if (typeof (num1, num2) == "number" && num1 > 0 && num2 > 0) {
+		return Math.abs(sumUp(num1) - sumUp(num2)) + Math.min(num2, num1);
+	}
+	return "ERROR";
+};
 
-module.exports = sumAll
+module.exports = sumAll;
