cb617fcd89510ff542d0d6ef75ffee160382728c
Ominini <omininiab@gmail.com>
2021-01-08 12:07:17 -0500

    completed task 3: reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    reversedWord = ''
+    for (let i = (word.length - 1); i >= 0; i--) {
+        reversedWord += word[i];
+    }
+    return(reversedWord);
 }
-
+console.log(reverseString('word'));
 module.exports = reverseString
