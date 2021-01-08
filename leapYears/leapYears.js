3d884977b08d1ca91b068ab2df567339898fbf9b
Ominini <omininiab@gmail.com>
2021-01-08 15:38:25 -0500

    completed task 6: leapYears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
+const leapYears = function (yr) {
+	if (yr % 400 == 0 || (yr % 100 > 0 && yr % 4 == 0)) {
+		return true;
+	}
+	return false;
+};
 
-}
-
-module.exports = leapYears
+module.exports = leapYears;
