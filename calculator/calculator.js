82b3a6722f77f82141cc139368e855064930438f
Ominini <omininiab@gmail.com>
2021-01-10 15:42:16 -0500

    completed

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,32 @@
-function add () {
-	
+function add(...x) {
+  return x.reduce((a, b) => Number(a) + Number(b), 0);
 }
 
-function subtract () {
-	
+function subtract(...x) {
+  return x.reduce((a, b) => Number(a) - Number(b));
 }
 
-function sum () {
-	
+function sum(x) {
+  return add(...x);
 }
 
-function multiply () {
-	
+function multiply(x) {
+	return x.reduce((a,b)=>a*b);
+  }
+
+function power(a, b) {
+  return a ** b;
 }
 
-function power() {
-	
-}
-
-function factorial() {
-	
+function factorial(x) {
+  return x==0 ? 1 : multiply([...Array(x + 1).keys()].slice(1));
 }
 
 module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
-    power,
-	factorial
-}
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial,
+};
