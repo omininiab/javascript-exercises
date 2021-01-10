873d879c0a4b2c37bb77141bc1df4891434f9a5b
Ominini <omininiab@gmail.com>
2021-01-10 17:44:01 -0500

    completed

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function() {
-
+function charsOnly(string) {
+  return string.replace(/[\W_]+/g,"").toLowerCase();
 }
 
-module.exports = palindromes
+const palindromes = function (text) {
+    text=charsOnly(text);
+
+    firstHalf = text.slice(0,Math.floor(text.length/2))
+    secondHalf = text.slice(Math.round(text.length/2)).split("")
+    secondHalf=secondHalf.reverse().join("")
+    return firstHalf==secondHalf
+};
+
+module.exports = palindromes;
