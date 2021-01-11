63f4a3dca2579eb9a689ba9be81ff7e4a5fc3d5f
Ominini <omininiab@gmail.com>
2021-01-11 05:51:53 -0500

    completed

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0){return "OOPS"}
+    /*givenF = [1,1]
+    for (i=0;i<num;i++) {
+        givenF.push(givenF[givenF.length-1] + givenF[givenF.length-2])
+    }
+    return givenF[num-1]*/
+    return parseInt(((((1+Math.sqrt(5))/2)**num) - (((1-Math.sqrt(5))/2)**num))/Math.sqrt(5))
 }
 
 module.exports = fibonacci
