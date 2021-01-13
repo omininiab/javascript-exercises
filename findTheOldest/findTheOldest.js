5774e9fbf67bfc1aec1f1041f27a9e7b3bd2e066
Ominini <omininiab@gmail.com>
2021-01-12 19:03:33 -0500

    completed

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-let findTheOldest = function() {
+age = function (yearOfBirth, yearOfDeath = 0) {
+  if (yearOfDeath == 0) {
+    d = new Date();
+    return d.getFullYear() - yearOfBirth;
+  }
+  return yearOfDeath - yearOfBirth;
+};
 
-}
+let maxCallback2 = (max, cur) => Math.max(max, cur);
 
-module.exports = findTheOldest
+let findTheOldest = function (people) {
+  people.map(
+    (person) => (person.age = age(person.yearOfBirth, person.yearOfDeath))
+  );
+
+  return people.reduce((oldest, person) =>
+    oldest.age > person.age ? oldest : person
+  );
+};
+
+module.exports = findTheOldest;
